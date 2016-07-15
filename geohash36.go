1ce33b101ab7cad982d4c1b2cd6634c8e6410daa
myyang <ymy1019@gmail.com>
2016-07-15 18:47:31 +0800

    Add GeoHash36 cryptor

--- geohash36.go
+++ geohash36.go
@@ -2,22 +2,23 @@ package geohash
 
 import "bytes"
 
+// fix constants
 const (
-	initUnitLat = 30.0
-	initUnitLng = 60.0
+	initUnitLat   = 30.0
+	initUnitLng   = 60.0
+	DefaultB36Str = "23456789bBCdDFgGhHjJKlLMnNPqQrRtTVWX"
 )
 
 // Helper variables
 var (
-	B36 = []byte("23456789bBCdDFgGhHjJKlLMnNPqQrRtTVWX")
+	B36 = []byte(DefaultB36Str)
 )
 
-// Encode36 provides geohash-36 encode
-// return the encoded string, latErr, lngErr in degree
-func Encode36(latitude, longitude float64, precision int) (string, float64, float64) {
+func encode36(latitude, longitude float64, key []byte, precision int) (
+	hash string, maxLat, minLat, maxLng, minLng, unitLat, unitLng float64) {
 	b := make([]byte, 0, precision)
-	unitLng, maxLng, minLng := initUnitLng, MaxLng, MinLng
-	unitLat, maxLat, minLat := initUnitLat, MaxLat, MinLat
+	unitLng, maxLng, minLng = initUnitLng, MaxLng, MinLng
+	unitLat, maxLat, minLat = initUnitLat, MaxLat, MinLat
 	for i := 0; i < precision; i++ {
 		// since max search time is 12 (6 for each),
 		// just iterate it instead of binary search
@@ -32,21 +33,21 @@ func Encode36(latitude, longitude float64, precision int) (string, float64, floa
 		}
 		// update square
 		minLat, maxLng = maxLat-unitLat, minLng+unitLng
-		unitLat, unitLng = unitLat/6.0, unitLng/6.0
-		b = append(b, B36[clat*6+clng])
+		unitLat, unitLng = unitLat/6, unitLng/6
+		b = append(b, key[clat*6+clng])
 	}
 	_, _ = (minLat+maxLat)/2, (minLng+maxLng)/2
-	return string(b[:]), unitLat, unitLng
+	return string(b[:]), maxLat, minLat, maxLng, minLng, roundFloat64(unitLat, precision), roundFloat64(unitLng, precision)
 }
 
-// Decode36 decode given hash value into latitude and longitude centry point
-func Decode36(hashv string, precision int) (float64, float64) {
+func decode36(hashv string, key []byte, precision int) (
+	lat, lng, maxLat, minLat, maxLng, minLng, uLat, uLng float64) {
 	hashb := []byte(hashv)
 	if precision <= 0 {
 		precision = len(hashb)
 	}
-	uLat, maxLat, minLat := initUnitLat, MaxLat, MinLat
-	uLng, maxLng, minLng := initUnitLng, MaxLng, MinLng
+	uLat, maxLat, minLat = initUnitLat, MaxLat, MinLat
+	uLng, maxLng, minLng = initUnitLng, MaxLng, MinLng
 	for _, v := range hashb {
 		i := bytes.IndexByte(B36, v)
 		row, col := i/6, i%6
@@ -56,5 +57,101 @@ func Decode36(hashv string, precision int) (float64, float64) {
 		maxLng = minLng + uLng
 		uLat, uLng = uLat/6, uLng/6
 	}
-	return roundFloat64((maxLat+minLat)/2, precision), roundFloat64((maxLng+minLng)/2, precision)
+	return roundFloat64((maxLat+minLat)/2, precision), roundFloat64((maxLng+minLng)/2, precision),
+		maxLat, minLat, maxLng, minLng, uLat, uLng
+}
+
+// NewGeoHash36WithDefaultKey generate geohash36 cryptor with default key
+func NewGeoHash36WithDefaultKey() GeoCryptor {
+	return NewGeoHash36(DefaultB36Str)
+}
+
+// NewGeoHash36 generate geohash36 with given key
+func NewGeoHash36(key string) GeoCryptor {
+	g := &GeoHash36{}
+	g.SetKey(key)
+	return g
+}
+
+// GeoHash36 is one of GeoCryptor that provide geohash36 coding
+// for more detai, please check wiki page:
+// https://en.wikipedia.org/wiki/Geohash-36
+// or promotion website: http://geo36.org
+type GeoHash36 struct {
+	key []byte
+}
+
+// SetKey set hash key value
+func (g *GeoHash36) SetKey(key string) {
+	g.key = []byte(key)
+}
+
+// HashKey return hash key of this hasher
+func (g *GeoHash36) HashKey() string {
+	return string(g.key)
+}
+
+// Encode and return hash value only
+func (g *GeoHash36) Encode(latitude, longitude float64, precision int) string {
+	v, _, _, _, _, _, _ := encode36(latitude, longitude, g.key, precision)
+	return v
+}
+
+// Decode and return central lat, lng pair
+func (g *GeoHash36) Decode(value string, precision int) (float64, float64) {
+	lat, lng, _, _, _, _, _, _ := decode36(value, g.key, precision)
+	return lat, lng
+}
+
+// EncodeWithErr returns also estimate error in degree
+func (g *GeoHash36) EncodeWithErr(latitude, longitude float64, precision int) (string, float64, float64) {
+	v, _, _, _, _, latErr, lngErr := encode36(latitude, longitude, g.key, precision)
+	return v, latErr * 6, lngErr * 6
+}
+
+// DecodeWithErr returns also estimate error
+func (g *GeoHash36) DecodeWithErr(value string, precision int) (float64, float64, float64, float64) {
+	lat, lng, _, _, _, _, latErr, lngErr := decode36(value, g.key, precision)
+	return lat, lng, latErr * 6, lngErr * 6
+}
+
+// EncodeAsBox returns a location box
+func (g *GeoHash36) EncodeAsBox(latitude, longitude float64, precision int) BoundingBox {
+	v, maxlat, minlat, maxlng, minlng, latErr, lngErr := encode36(latitude, longitude, g.key, precision)
+	return &LocationBox{
+		MaxLat: maxlat, MinLat: minlat, MaxLng: maxlng, MinLng: minlng,
+		LatErr: latErr * 6, LngErr: lngErr * 6, Hash: v, Precision: precision}
+}
+
+// DecodeAsBox returns a location box
+func (g *GeoHash36) DecodeAsBox(value string, precision int) BoundingBox {
+	_, _, maxlat, minlat, maxlng, minlng, latErr, lngErr := decode36(value, g.key, precision)
+	return &LocationBox{
+		MaxLat: maxlat, MinLat: minlat, MaxLng: maxlng, MinLng: minlng,
+		LatErr: latErr * 6, LngErr: lngErr * 6, Hash: value, Precision: precision}
+}
+
+// Neighbors returns adjcent 8 neighbors
+func (g *GeoHash36) Neighbors(value string, precision int) []BoundingBox {
+	lb := g.DecodeAsBox(value, precision)
+	neighbor := func(lb *LocationBox, dlat, dlng int) *LocationBox {
+		latUnit, lngUnit := (lb.MaxLat-lb.MinLat)*float64(dlat), (lb.MaxLng-lb.MinLng)*float64(dlng)
+		r := &LocationBox{
+			MaxLat: lb.MaxLat + latUnit, MinLat: lb.MinLat + latUnit,
+			MaxLng: lb.MaxLng + lngUnit, MinLng: lb.MinLng + lngUnit,
+			LatErr: lb.LatErr, LngErr: lb.LngErr}
+		lat, lng := r.GetCenter()
+		r.Hash = g.Encode(lat, lng, precision)
+		return r
+	}
+	n := make([]BoundingBox, 0, 8)
+	for i := -1; i < 2; i++ {
+		for j := -1; j < 2; j++ {
+			if i == 0 && j == 0 {
+				continue
+			}
+			n = append(n, neighbor(lb.(*LocationBox), i, j))
+		}
+	}
+	return n
 }
--- geohash36_test.go
+++ geohash36_test.go
@@ -3,18 +3,37 @@ package geohash
 import (
 	"fmt"
 	"path/filepath"
-	"reflect"
 	"runtime"
 	"testing"
 )
 
+var degreeErr = []struct {
+	LatErr float64
+	LngErr float64
+}{
+	{0, 0},
+	{30.0, 60.0},
+	{5.0, 10.0},
+	{0.833, 1.667},
+	{0.1389, 0.2778},
+	{0.02315, 0.0463},
+	{0.003858, 0.007716},
+	{0.000643, 0.001286},
+	{0.00010717, 0.00021433},
+	{1.7861e-05, 3.5722e-05},
+	{2.9769e-06, 5.9537e-06},
+	{4.9615e-07, 9.9229e-07},
+	{8.2691e-08, 1.65382e-07},
+}
+
 func TestEncode36(t *testing.T) {
+	cryptor := NewGeoHash36WithDefaultKey()
 	exp, got := "", ""
 
 	exp = "bdrdC26BqH"
-	got, _, _ = Encode36(51.504444, -0.086666, 10)
+	got = cryptor.Encode(51.504444, -0.086666, 10)
 
-	if !reflect.DeepEqual(exp, got) {
+	if exp != got {
 		_, file, line, _ := runtime.Caller(0)
 		fmt.Printf(
 			"%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\n\n",
@@ -23,9 +42,9 @@ func TestEncode36(t *testing.T) {
 	}
 
 	exp = "bdrdC26"
-	got, _, _ = Encode36(51.504444, -0.086666, 7)
+	got = cryptor.Encode(51.504444, -0.086666, 7)
 
-	if !reflect.DeepEqual(exp, got) {
+	if exp != got {
 		_, file, line, _ := runtime.Caller(0)
 		fmt.Printf(
 			"%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\n\n",
@@ -34,22 +53,24 @@ func TestEncode36(t *testing.T) {
 	}
 
 	exp = "H2RXqLHNG6"
-	got, _, _ = Encode36(25.03297033, 121.56542031, 10)
+	got = cryptor.Encode(25.03297033, 121.56542031, 10)
 
-	if !reflect.DeepEqual(exp, got) {
+	if exp != got {
 		_, file, line, _ := runtime.Caller(0)
 		fmt.Printf(
 			"%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\n\n",
 			filepath.Base(file), line, exp, got)
 		t.FailNow()
 	}
+
 }
 
 func TestDecode36(t *testing.T) {
+	cryptor := NewGeoHash36WithDefaultKey()
 	lat, lng := 0.0, 0.0
 	explat, explng := 0.0, 0.0
 
-	lat, lng = Decode36("bdrdC26BqH", 6)
+	lat, lng = cryptor.Decode("bdrdC26BqH", 6)
 	explat, explng = 51.504444, -0.086666
 
 	if lat != explat || lng != explng {
@@ -60,7 +81,7 @@ func TestDecode36(t *testing.T) {
 		t.FailNow()
 	}
 
-	lat, lng = Decode36("H2RXqLHNG6", 8)
+	lat, lng = cryptor.Decode("H2RXqLHNG6", 8)
 	explat, explng = 25.03297033, 121.56542031
 
 	if lat != explat || lng != explng {
@@ -73,15 +94,17 @@ func TestDecode36(t *testing.T) {
 }
 
 func BenchmarkEncode36(b *testing.B) {
+	cryptor := NewGeoHash36WithDefaultKey()
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		Encode36(25.03297033, 121.56542031, 10)
+		cryptor.Encode(25.03297033, 121.56542031, 10)
 	}
 }
 
 func BenchmarkDecode36(b *testing.B) {
+	cryptor := NewGeoHash36WithDefaultKey()
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		Decode36("H2RXqLHNG6", 8)
+		cryptor.Decode("H2RXqLHNG6", 8)
 	}
 }
